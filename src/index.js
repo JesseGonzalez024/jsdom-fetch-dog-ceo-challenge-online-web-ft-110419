af3f6d18dec5b0c682b6d6e17f3523d719589004
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-02-26 01:53:33 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 function imageFetcher() {
   return fetch(imgUrl)
   .then(resp => resp.json())
-
-  
+  .then(json => renderImage(json))
 }
 
+function breedFetcher() {
+  return fetch(breedUrl)
+  .then(resp => resp.json())
+  .then(json => renderBreeds(json))
+}
+
+function renderImage(json) {
+  console.log(json.message)
+}
